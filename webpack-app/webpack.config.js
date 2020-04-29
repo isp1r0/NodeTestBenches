cd174dee024dfae2a159abddf8aeecb45ef907b1
Griffin Yourick <gryphon92@gmail.com>
2020-04-29 14:26:40 -0400

    Restore express webpack config, play with mode

--- webpack-app/webpack.config.js
+++ webpack-app/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 // const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  // mode: 'development',
   entry: {
     index: './index.js',
   },
